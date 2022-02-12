f109cc5a19bbac9e6361c6fb6454f03d207580f1
ankurp1511 <ankur.b2k12.iiita@gmail.com>
2022-02-12 21:39:44 +0530

    Leetcode-1 Solution using hashmap, O(n*2)

---
+++ Leetcode_1/src/Solution.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+     int[] result = twoSum(new int[]{2,7,11,15}, 9);
+     System.out.println(Arrays.toString(result));
+    }
+
+    private static int[] twoSum(int[] nums, int target) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i=0; i< nums.length; i++) {
+            if (map.containsKey(target-nums[i])) {
+                return new int[]{map.get(target- nums[i]), i};
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+        return new int[0];
+    }
+
+}
